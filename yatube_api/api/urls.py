e754b4c42e239a7297d0abe4d1e66f152f0eb362
nikitamoiseev <nikita-moiseev-2003@yandex.ru>
2025-03-31 14:40:49 +0300

    1

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, GroupViewSet, CommentViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'groups', GroupViewSet, basename='group')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
